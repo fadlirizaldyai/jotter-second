6c24e36542a432d493b6f671285f1de6a3afa0b1
fadlirizaldyai <fadly.rizaldy@ai.astra.co.id>
2024-06-21 10:04:10 +0700

    fix: detail with button

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -8,6 +8,43 @@ const DetailPage = () => {
   const data = getInitialData.filter((data) => data.id == id)[0];
   const navigate = useNavigate();
 
+  const handleClick = async () => {
+    const url = "https://dummy-project-beryl.vercel.app/api/auth";
+    // const url = "http://localhost:3000/api/auth"
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          Authorization: "TOKENDUMMY", // Replace with your actual token
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+      console.log("RESPOSE", response);
+
+      if (response.redirected) {
+        // router.push(response.url);
+        window.location.href = response.url; // Redirect to the URL provided by the response
+      }
+      // const data = await response.json();
+      // console.log("Response", response, data);
+      // if (data.redirected) {
+      // }
+
+      //   if (response.redirected) {
+      //     window.location.href = response.url;
+      //   }
+      // else {
+      //     const data = await response.json();
+      //     if (data.redirectUrl) {
+      //       window.location.href = data.redirectUrl;
+      //     }
+      //   }
+    } catch (error) {
+      console.log("ERROR", error);
+    }
+  };
+
   return (
     <Layout>
       <div className="px-4 mt-4">
@@ -35,6 +72,9 @@ const DetailPage = () => {
           </p>
           <p>{data?.body}</p>
         </section>
+        <button className="p-3 bg-blue-400" onClick={handleClick}>
+          Go to Host 3000
+        </button>
       </div>
     </Layout>
   );
